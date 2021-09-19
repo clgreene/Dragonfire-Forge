e852b00e1a625b677bd806d407b190aeff2c7244
Carter <clgaspect@gmail.com>
2021-09-18 19:04:41 -0600

    Scrolling, finding arrows, dissapearing

--- Dragon Fire Forge/Assets/Scripts/ArrowList.cs
+++ Dragon Fire Forge/Assets/Scripts/ArrowList.cs
@@ -8,6 +8,8 @@ public class ArrowList : MonoBehaviour
     public GameObject[] arrowList;
     public ListData arrowPositions;
     public int length;
+    public BellowsGame bellowsGame;
+    public int nextArrow;
     
 
     // Start is called before the first frame update
@@ -24,17 +26,26 @@ public class ArrowList : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+
+        //resetting position tracker to first item in list
         if (length == (arrowList.Length - 1))
         {
             arrowPositions.value.Clear();
             length = 0;
         }
 
+        //tracking position for each item in array
         while (length < (arrowList.Length - 1))
         {
             arrowPositions.value.Add(arrowList[length].transform.position);
             length++;
 
+        }
+
+        if (arrowList[nextArrow].transform.position.y < -.6 && nextArrow <= (arrowList.Length - 1))
+        {
+            arrowList[nextArrow].SetActive(false);
+            nextArrow++;
         }
     }
 }
--- Dragon Fire Forge/Assets/Scripts/BellowsGame.cs
+++ Dragon Fire Forge/Assets/Scripts/BellowsGame.cs
@@ -9,16 +9,18 @@ public class BellowsGame : MonoBehaviour
     public GameObject[] arrowGames;
     public GameObject currentGame;
     public int randomInt = 1;
+    public bool foundNextArrow;
+    public int nextArrow;
     public BoolData start;
     public BoolData playing;
     public int arrow = 0;
-    public Vector3 arrowPos;
     public int weaponMat;
+    public ListData arrowPositions;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        nextArrow = 0;
     }
 
     // Update is called once per frame
@@ -30,25 +32,33 @@ public class BellowsGame : MonoBehaviour
 
             
 
-            //pull arrow array/list
-            //locate arrow based on arrow number in array - object location
+            
             //if mousebuttondown then access arrow position, play arrow animation based on position, and hide arrow until off screen (unless game object resets itself when reused, then just hide arrow)
             //if arrow position is past too far position, play missed animation at position, and hide arrow
             //increase arrow number by one
-            //if arrow number is larger than list, start is false.
+            //if arrow number is larger than list, playing is false.
 
 
         }
         if (playing.value == true)
         {
             currentGame.transform.Translate(0, -1 * Time.deltaTime, 0);
-            //arrowPos = new Vector3(currentGame.GetComponent<arrowList(arrow);
+            
         }
+
+        //reset the next arrow for the buttons when arrow has travelled to far
+        if (arrowPositions.value[nextArrow].y < -.6 && playing == true)
+        {
+            
+            nextArrow++;
+        }
+
     }
 
     public IEnumerator initializeGame()
     {
         start.value = false;
+        arrowPositions.value.Clear();
         yield return new WaitForSeconds(0f);
         playing.value = true;
         randomInt = Random.Range(1, arrowGames.Length);
@@ -67,6 +77,26 @@ public class BellowsGame : MonoBehaviour
         //play start animation
 
 
+
+    }
+
+    public IEnumerator rightButton()
+    {
+        yield return new WaitForSeconds(0f);
+        nextArrow++;
+
+
+        //checkArrowDirection
+        //CompareArrowPosition to Best Position
+        //run animation / hide arrow
+        //calculate score and add score
+        //move to next arrow
+
+    }
+
+    public IEnumerator leftButton()
+    {
+        yield return new WaitForSeconds(0f);
 
     }
 
