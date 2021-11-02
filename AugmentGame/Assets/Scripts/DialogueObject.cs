0356c0858611bdf6f9b6921335c0c2024b9d6146
Carter <clgaspect@gmail.com>
2021-11-01 21:02:10 -0600

    interact Icon and better event scripts

--- AugmentGame/Assets/Scripts/DialogueObject.cs
+++ AugmentGame/Assets/Scripts/DialogueObject.cs
@@ -1,39 +1,63 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 
 public class DialogueObject : MonoBehaviour
 {
+    //series of events that interacting causes
+    public UnityEvent[] eventSeries;
     
+
+    //object components
+    public GameObject interactIcon;
+
+    //player complonents
+    GameObject player;
+    public BoolData interacting;
+
+
+    //common manager scripts for common use
+    DialogueRunner dialogueController;
     public List<StringData> dialogue;
     public IntData dialogueInstance;
 
-    DialogueRunner activeDialogue;
-
-    GameObject player;
-    
 
     // Start is called before the first frame update
     void Start()
     {
-        activeDialogue = FindObjectOfType<DialogueRunner>();
-        player = FindObjectOfType<EVE>();
-        
+        dialogueController = FindObjectOfType<DialogueRunner>();
+        player = GameObject.FindGameObjectWithTag("Player");
+ 
     }
 
-    // Update is called once per frame
-    void Update()
+    public void OnCollisionEnter2D(Collision2D collision)
     {
-        
-    }
-
-    public void OnCollisionStay2D(Collider2D other)
-    {
-        if (other.tag == "Player")
+        if (collision.gameObject.tag == "Player")
         {
-            //enable button icon
-            //
+            interactIcon.SetActive(true);
+            interacting.value = true;
+
         }
     }
+
+    public void OnCollisionStay2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            
+        }
+    }
+
+    public void OnCollisionExit2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            interactIcon.SetActive(false);
+            interacting.value = false;
+        }
+    }
+
+
 
 }
--- AugmentGame/Assets/Scripts/DialogueRunner.cs
+++ AugmentGame/Assets/Scripts/DialogueRunner.cs
@@ -4,6 +4,11 @@ using UnityEngine;
 
 public class DialogueRunner : MonoBehaviour
 {
+
+    public StringData activeDialogue;
+
+
+
     // Start is called before the first frame update
     void Start()
     {
